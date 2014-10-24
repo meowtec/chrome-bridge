0f2cb646ed4124918944b083121f6d45c121d36a
meowtec <sairly@126.com>
2014-10-25 00:59:00 +0800

    chrome extension 测试

--- test/content.js
+++ test/content.js
@@ -1,4 +1,24 @@
+function equal(name, a, b){
+  if(a === b){
+    console.log(name, true )
+  }else{
+    console.error(name, false, a, b)
+  }
+}
 
-exec(function(){
-
-})
+// 最复杂的情况
+var execfunc = function () {
+  postDataBack(pageVar1, 'reciever1')
+  equal('传入参数', strrrrr, 'content var 1')
+  equal('传入dom', body, document.body)
+}
+var execCallbacks = {
+  'reciever1': function (str) {
+    equal('返回值', str, 'abc')
+  }
+}
+var execVars = {
+  'strrrrr': 'content var 1',
+  'body': document.body
+}
+exec(execfunc, execCallbacks, execVars)
---
+++ test/index.js
@@ -0,0 +1,9 @@
+var pageVar1 = 'abc'
+
+function equal(name, a, b){
+  if(a === b){
+    console.log(name, true )
+  }else{
+    console.error(name, false, a, b)
+  }
+}
