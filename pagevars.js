fcc7e05dcbf8e62124c9dbef0f62e11a7cb67033
meowtec <sairly@126.com>
2014-11-28 11:46:25 +0800

    callbackData element fix

--- pagevars.js
+++ pagevars.js
@@ -24,15 +24,9 @@ window.exec = (function () {
     //获取function内部代码字符串
     getScriptInner: function (func) {
       return '\n' + func.toString().replace(/^.*\{\s*/, '').replace(/\s*\}$/, '') + '\n'
-    }
-  }
+    },
 
-  // id, 用来绑定 exec(script) 中返回的数据
-  var datasetName = 'elodia' + utils.randstring()
-
-  var callbacksDict = {};
-  var contentPreScript = utils.getScriptInner(function () {
-    var stringifyArr = function(arr){
+    stringifyArr: function(arr){
       var str = '['
       for(var i = 0; i<arr.length; i++){
         var item = arr[i]
@@ -47,45 +41,28 @@ window.exec = (function () {
       str = str.replace(/,\s*$/, '') + ']'
       return str
     }
-    var callbackData = function(){
-      document.body.dataset['{{datasetName}}'] = JSON.stringify({
-        rid: '{{rid}}',
-        data: stringifyArr(arguments)
-      })
-    }
-  })
+  }
 
-  // defineVars
-  function defineVars(object) {
-    var obj = object || {}
-    var code = ''
-    for (var key in obj) {
-      var value = obj[key]
-      if (value instanceof HTMLElement) {
-        var randAttr = utils.randstring()
-        value.setAttribute('elodia-elem-' + randAttr, '')
-        code = code + 'var ' + key + ' = document.querySelector(\'[elodia-elem-' + randAttr + ']\');'
-      } else {
-        code = code + 'var ' + key + ' = JSON.parse(' + JSON.stringify(value) + ');'
+  // id, 用来绑定 exec(script) 中返回的数据
+  var datasetName = 'elodia' + utils.randstring()
+
+  var callbacksDict = {};
+
+  var contentPreScript = utils.getScriptInner(function () {
+    var callbackData = (function(){
+      var utils = {}
+      utils.randstring = function (){}
+      utils.stringifyArr = function (){}
+      return function(){
+        document.body.dataset['{{datasetName}}'] = JSON.stringify({
+          rid: '{{rid}}',
+          data: utils.stringifyArr(arguments)
+        })
       }
-    }
-    return code
-  }
-  var stringifyArr = function(arr){
-    var str = '['
-    for(var i = 0; i<arr.length; i++){
-      var item = arr[i]
-      if(item instanceof HTMLElement){
-        var randAttr = utils.randstring()
-        item.setAttribute('elodia-elem-' + randAttr, '')
-        str = str + 'document.querySelector(\'[elodia-elem-' + randAttr + ']\'), '
-      }else{
-        str = str + JSON.stringify(item) + ', '
-      }
-    }
-    str = str.replace(/,\s*$/, '') + ']'
-    return str
-  }
+    })()
+  }).replace('randstring = function (){}', 'randstring = ' + utils.randstring.toString())
+    .replace('stringifyArr = function (){}', 'stringifyArr = ' + utils.stringifyArr.toString())
+
 
   // 监听属性变化
   var observer = new MutationObserver(function (mutations) {
@@ -117,7 +94,7 @@ window.exec = (function () {
     var id = 'id' + parseInt(Math.random() * 10000000000000)
     // 去 function name
     var scriptText = func.toString().replace(/function.*?\(/, 'function(').replace(/\{/, '{'+contentPreScript+';')
-    var script = '(' + scriptText + ').apply(null, ' + stringifyArr(args) + ')'
+    var script = '(' + scriptText + ').apply(null, ' + utils.stringifyArr(args) + ')'
     script = script.replace('{{rid}}', id).replace('{{datasetName}}', datasetName)
     utils.evaluate(script)
     return function(callback){
